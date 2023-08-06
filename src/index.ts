2f49fbc0e360d23e025e92d3330843efa1412378
DeboDevelop <debajyotidofficial@gmail.com>
2023-08-06 15:04:52 +0530

    feat: Add express server with typescript

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+    res.json('Hello World!!!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on PORT ${port}`);
+});
