998571f72f2ffc75b0bc364a2020e2376521f592
DeboDevelop <debajyotidofficial@gmail.com>
2023-08-10 15:44:02 +0530

    fix: Add ssl true for prod env

--- src/database.ts
+++ src/database.ts
@@ -1,6 +1,6 @@
-import { Pool } from "pg";
+import { Pool, PoolConfig } from "pg";
 
-const pool = new Pool({
+const poolConfig: PoolConfig = {
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database:
@@ -9,6 +9,11 @@ const pool = new Pool({
             : process.env.DB_NAME,
     host: process.env.DB_HOST_NAME,
     port: 5432, // PostgreSQL default port
-});
+};
+if (process.env.NODE_ENV === "prod") {
+    poolConfig.ssl = true;
+}
+
+const pool = new Pool(poolConfig);
 
 export default pool;
